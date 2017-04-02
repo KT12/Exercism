79a790b56cfbd72338bf88684234659e02116405
KT12 <nyy2008@gmail.com>
2017-04-02 17:37:57 -0400

    Solved

--- python/rail-fence-cipher/rail_fence_cipher.py
+++ python/rail-fence-cipher/rail_fence_cipher.py
@@ -6,8 +6,23 @@ Created on Tue Jan 17 14:05:02 2017
 @author: ktt
 """
 
-def encode(msg):
-    pass
+# Got stuck on this one for a while
 
-def decode(ss):
-    pass
+from itertools import cycle
+
+
+def encode(msg, h):
+    positions = pattern(h, len(msg))
+    return ''.join(msg[i] for _, i in positions)
+
+
+def decode(fence, h):
+    positions = pattern(h, len(fence))
+    rectified = sorted(enumerate(positions), key=lambda t: t[1][1])
+    return ''.join(fence[i] for i, _ in rectified)
+
+
+def pattern(h, w):
+    n = 2 * (h - 1)
+    positions = (min(x, n - x) for x in cycle(range(n)))
+    return sorted(zip(positions, range(w)))
