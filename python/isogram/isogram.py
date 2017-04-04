d810c7460310be7d6c4472fec651663db9ff8241
KT12 <nyy2008@gmail.com>
2017-04-04 12:33:04 -0400

    Solve problem

--- python/isogram/isogram.py
+++ python/isogram/isogram.py
@@ -1,2 +1,11 @@
-def is_isogram():
-    pass
+from collections import Counter
+
+def is_isogram(phrase):
+    # strip away all non-alphabetical characters
+    phrase = ''.join(ch for ch in phrase if ch.isalpha())
+    # empty space is considered isogram, so len is > 1, not == 1
+    # this would not catch the case where all letters are only doubled, 3x etc
+    if len(set(Counter(list(phrase.lower())).values())) > 1:
+        return False
+    else:
+        return True
