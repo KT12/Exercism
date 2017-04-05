6f5b4006b5d3830b545226c7e287f7e095071b95
KT12 <nyy2008@gmail.com>
2017-04-05 11:41:59 -0400

    Solve for robust multiple cases

--- python/isogram/isogram.py
+++ python/isogram/isogram.py
@@ -2,10 +2,19 @@ from collections import Counter
 
 def is_isogram(phrase):
     # strip away all non-alphabetical characters
-    phrase = ''.join(ch for ch in phrase if ch.isalpha())
-    # empty space is considered isogram, so len is > 1, not == 1
-    # this would not catch the case where all letters are only doubled, 3x etc
-    if len(set(Counter(list(phrase.lower())).values())) > 1:
+    ph = ''.join(ch for ch in phrase if ch.isalpha())
+    char_count = set(Counter(list(ph.lower())).values())
+    # empty space is considered isogram
+    # so char_count = 1 or 0 is an isogram
+    
+    # case of multiple occurrences of letters
+    if len(char_count) > 1:
+        return False
+    # case of empty string as isogram
+    elif len(char_count) == 0:
+        return True
+    # case of only doubled or tripled letters
+    elif list(char_count)[0] != 1:
         return False
     else:
         return True
